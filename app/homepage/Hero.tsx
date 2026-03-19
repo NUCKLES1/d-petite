96389af4c7ac820dafe685ab67e1771b843a7219
Nuckles1 <danyxcharge@gmail.com>
2026-03-19 11:35:28 +0100

    main

--- app/homepage/Hero.tsx
+++ app/homepage/Hero.tsx
@@ -6,7 +6,7 @@ import  BackgroundVideo from "next-video/background-video";
 const Hero = () => {
   return (
     <div>
-      <section className="relative h-[80dvh] lg:h-screen w-full overflow-hidden bg-[#fafaf8]">
+      <section className="relative h-[80dvh] lg:h-screen w-full overflow-hidden bg-[#fafaf8] rounded-bl-2xl rounded-br-sm">
         {/* Background Image with Parallax Effect */}
         <motion.div
           className="absolute inset-0 h-[80vh]"
@@ -25,7 +25,7 @@ const Hero = () => {
            <BackgroundVideo src="https://res.cloudinary.com/dbc4muo4t/video/upload/v1773756541/0317_vm4qmj.mp4" className='h-[80dvh] max-lg:h-[80vh] ' autoPlay muted playsInline loop={false} height={620}/>
            </div>
           <div className="h-[80vh] lg:hidden md:hidden">
-           <BackgroundVideo src="https://res.cloudinary.com/dbc4muo4t/video/upload/v1773762872/0317_iwrye6.mp4" className='h-[80dvh]'preload="auto" autoPlay muted playsInline loop={false} height={620}/>
+           <BackgroundVideo src="https://res.cloudinary.com/dbc4muo4t/video/upload/v1773762872/0317_iwrye6.mp4" className='h-[80dvh] 'preload="auto" autoPlay muted playsInline loop={false} height={620}/>
            </div>
         </motion.div>
 
